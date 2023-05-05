f0141304dadfafbea4c29c98234f8b06348d7846
Osamu Ogasawara <osamu.ogasawara@gmail.com>
2023-05-05 17:58:25 +0900

    Specify library versions in mvn_repos.sh.

--- mvn_repos.sh
+++ mvn_repos.sh
@@ -2,19 +2,21 @@
 
 
 function mvn_install() {
-    cd ..
-    git clone https://github.com/oogasawa/$1 
+    cd /tmp
+    git clone --branch $2 --depth 1 https://github.com/oogasawa/$1 
     cd $1   
     mvn clean compile install
+    cd ..
+    rm -Rf $1
 }
 
-mvn_install POJO-bdd
-mvn_install Utility-cli
-mvn_install Utility-files
-mvn_install Utility-types
-mvn_install Pipe
-mvn_install DataCell
-mvn_install POJO-actor
-mvn_install autonomous_blastdb_updator
+mvn_install POJO-bdd       v1.4.2
+mvn_install Utility-cli    v1.0.0
+mvn_install Utility-files  v0.9.0
+mvn_install Utility-types  v0.11.0
+mvn_install Pipe           v1.2.0
+mvn_install DataCell       v1.2.3
+mvn_install POJO-actor     v1.2.0
+mvn_install autonomous_blastdb_updator v0.3.0
 
 
